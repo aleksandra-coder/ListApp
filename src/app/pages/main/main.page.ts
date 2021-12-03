e90b71581fe526cb5e6b71d9b906c448629feca6
aleksandra-coder <aleksandra-coder@users.noreply.github.com>
2021-12-03 12:23:12 +0200

    further styling and building main page

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
+
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
 })
+
 export class MainPage implements OnInit {
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
 }
