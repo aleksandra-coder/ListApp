dad6e0e292d4b81271b288ca3fddd18e4651d26d
Ana Hango <2005095@laurea.fi>
2021-12-06 12:24:45 +0200

    commit master

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
+  let splashScreenSpy, platformReadySpy, platformSpy;
+
   beforeEach(waitForAsync(() => {
+    splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
+    platformReadySpy = Promise.resolve();
+    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: SplashScreen, useValue: splashScreenSpy },
+        { provide: Platform, useValue: platformSpy },
+      ],
     }).compileComponents();
   }));
 
@@ -18,6 +27,14 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+  it('should initialize the app', waitForAsync () => {
+    TestBed.createComponent(AppComponent);
+    expect(platformSpy.ready).toHaveBeenCalled();
+    await platformReadySpy;
+    expect(statusBarSpy.styleDefault).toHaveBeenCalled();
+    expect(splashScreenSpy.hide).toHaveBeenCalled();
+  });
+
   // TODO: add more tests!
 
 });
