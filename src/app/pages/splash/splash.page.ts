61d5edd896d1f02cf694bbf2e924c44055446ff9
Carita Carstensen <carita.carstensen@gmail.com>
2021-12-10 19:51:09 +0200

    I modified the routing <3

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    setTimeout(()=>{
+       this.router.navigate(['loader']);
+    }, 2000)
+   
   }
 
 }
